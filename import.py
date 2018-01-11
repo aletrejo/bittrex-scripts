265724cf3d6d19fbdbe5ac5b84484ab5641d93e9
Benoit Hirbec <bhirbec@twitter.com>
2018-01-11 11:40:16 -0800

    Feedback from Ben

--- import.py
+++ import.py
@@ -1,52 +1,49 @@
 import requests
 import argparse
-from sqlalchemy import (create_engine, MetaData, Table, Column, Integer, String, Float, insert, delete)
-from common import dal, ht
+from sqlalchemy import delete
+
+from common import db_connect, market_summaries, PATH_HELP, DEBUG_HELP
+
 
 def get_market_data():
-    response = requests.get("https://bittrex.com/api/v1.1/public/getmarketsummaries")
+  response = requests.get("https://bittrex.com/api/v1.1/public/getmarketsummaries")
 
-    if response.status_code == 200:
-        py_response = response.json()
-        return py_response
-    else:
-        print("Something went wrong with the Bittrex API call.")
-        return None 
-
-
-def populate_db(data):
-    #Clear database contents
-    clear_db = delete(dal.market_summaries)
-    result = dal.connection.execute(clear_db)
-
-    #Insert data into database
-    if data is not None:
-        ins = dal.market_summaries.insert()
-        result = dal.connection.execute(ins, data["result"])
-        print("Success! The Bitcoin market data has been stored in the database.")
-    else:
-        print("Error. No data to store.")
+  if response.status_code == 200:
+    return response.json()
+  else:
+    print("Something went wrong with the Bittrex API call.")
+    return None
 
 
-def main():
-    #Command line arguments configuration
-    parser = argparse.ArgumentParser(description = "This script imports cryptocurrencies prices from the Bittrex API into a SQLite database.", formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("database_path", help=ht.path_help, type=str)
-    parser.add_argument("-d", "--debug", help=ht.debug_help, action = "store_true")
-    args = parser.parse_args()
-    path = "sqlite:///{}".format(args.database_path)
+def populate_db(conn, data):
+  # clear database contents
+  clear_db = delete(market_summaries)
+  result = conn.execute(clear_db)
 
-    #Initialize database 
-    if args.debug:
-        echo = True
-        dal.db_init(path, echo)
-    else:
-        echo = False
-        dal.db_init(path, echo)
+  # insert data into database
+  if data is not None:
+    ins = market_summaries.insert()
+    result = conn.execute(ins, data["result"])
+    print("Success! The Bitcoin market data has been stored in the database.")
+  else:
+    print("Error. No data to store.")
 
-    market_data = get_market_data()
-    populate_db(market_data)
+
+def main(args):
+  # database init
+  path = "sqlite:///{}".format(args.database_path)
+  conn = db_connect(path, args.debug)
+
+  market_data = get_market_data()
+  populate_db(conn, market_data)
+
+
+USAGE = '''This script imports cryptocurrencies prices from the Bittrex API
+into a SQLite database.'''
 
 
 if __name__ == "__main__":
-    main()
+  parser = argparse.ArgumentParser(description=USAGE , formatter_class=argparse.RawTextHelpFormatter)
+  parser.add_argument("database_path", help=PATH_HELP, type=str)
+  parser.add_argument("-d", "--debug", help=DEBUG_HELP, action = "store_true")
+  main(parser.parse_args())
